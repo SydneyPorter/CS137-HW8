2efc0f47281c6c37fbce7f24fe91972de5ee1cbb
Sydney Porter <sydneymport@gmail.com>
2022-11-15 18:54:05 -0600

    Rewrote code (see description)
    I'm assuming an issue with the code is that between lines 46-63 needing an "if touch[i] == 1" somewhere. Is this correct?

--- PorterHW8.py
+++ PorterHW8.py
@@ -1,99 +1,82 @@
 import math
 
-
-class Edge:
-
-    def __init__(self, start, end, weight):
-        if(start == end and weight != 0):
-            raise IndexError("No self-edges allowed")
-        self.__s = start # An integer representing a vertex index
-        self.__e = end # An integer representing a vertex index
-        self.__w = weight #An integer representing an edge weight between the vertices
-
-    def getWeight(self):
-        return self.__w
-
-    def getStart(self):
-        return self.__s
-
-    def getEnd(self):
-        return self.__e
-    
-    def __repr__(self):
-        return str(self.__s) + " --("+ str(self.__w) +")--> " +str(self.__e)
-
 class Graph:
-    def __init__(self,n):
-        self.__numVertices = n
-        self.__matrix = [ [ "inf" for i in range(n) ] for j in range(n) ] 
-
-    def setEdge(self,e):
-        self.__matrix[e.getStart()-1][e.getEnd()-1] = e.getWeight()
-
-    def getEdge(self,e):
-        return self.__matrix[e.getStart()-1][e.getEnd()-1]
-
-    def getNumVert(self):
-        return self.__numVertices
-
-    def __repr__(self):
-        return str(self.__matrix)
-
-    def __str__(self):
-        return '\n'.join([','.join(str(c) for c in row) for row in self.__matrix])
-
-def Dijkstra(W):
-    F = None
-    n = W.getNumVert()
-    i = None 
-    vnear = None
-    e = None
-    touch = [None] * n
-    length = [None] * n
-
-    i = 2
-    for i in range(n):
-        touch[i] = 1
-        length[i] = W.getEdge(i).weight
     
-    i = 0
-    for i in range(n-1):
+    def __init__(self, n):
+        self.n = n
+        self.numEdges = 0
+        self.edges = []
+    
+    def setEdge(self, edge):
+        self.edges.append(edge)
+        self.numEdges += 1
         
+    def getWeight(self, start, end):
+        i = 0
+        for i in range(self.numEdges):
+            temp = self.edges[i]
+            if temp.start == start and temp.end == end:
+                return temp.weight
+            i += 1
+        return 0
+        
+class Edge:
+    def __init__(self, start, end, weight):
+        self.start = start
+        self.end = end
+        self.weight = weight
+    
+    def __repr__(self):
+        return str([self.start, self.end, self.weight])
+    
+def Dijkstra(graph):
+    touch = [0] * graph.n
+    length = [0] * graph.n
+    newGraph = []
+    
+    i = 2
+    for i in range(graph.n):
+        if i == 2:
+            touch[i] = 1
+            length[i] = graph.getWeight(1,i)
+        i += 1
+    
+    i = 0  
+    for i in range(graph.n - 1):
         min = math.inf
+        
         j = 2
-        for j in range(n):
+        for j in range(graph.n):
             if 0 <= length[j] < min:
                 min = length[j]
                 vnear = j
-        e = W.getEdge(touch[vnear])
-        F.append(e)
+            j += 1
+
+        e = [touch[vnear], vnear]
+        newGraph.append(e)
         
         j = 2
-        for j in range(n):
-            if length[vnear] + W.weight[j] < length[j]:
-                length[i] = length[vnear] + W.weight[j]
+        for j in range(graph.n):
+            if length[vnear] + graph.getWeight(vnear, j) < length[j]:
+                length[j] = length[vnear] + graph.getWeight(vnear,j)
                 touch[j] = vnear
-                
+            j+= 1
+        
         length[vnear] = -1
+        i += 1
 
-    return F
-    
-# Example - Neapolitan p. 171
-W = Graph(5)
+    return newGraph
 
-W.setEdge(Edge(1,5,1))
-W.setEdge(Edge(1,4,6))
-W.setEdge(Edge(1,3,4))
-W.setEdge(Edge(1,2,7))
-W.setEdge(Edge(3,2,2))
-W.setEdge(Edge(3,4,5))
-W.setEdge(Edge(4,2,3))
-W.setEdge(Edge(5,4,1))
+Example = Graph(5)
 
-print(Edge(4,5,9))
+Example.setEdge(Edge(1,2,7))
+Example.setEdge(Edge(1,3,4))
+Example.setEdge(Edge(1,4,6))
+Example.setEdge(Edge(1,5,1))
+Example.setEdge(Edge(3,2,2))
+Example.setEdge(Edge(3,4,5))
+Example.setEdge(Edge(4,2,3))
+Example.setEdge(Edge(5,4,1))
 
-for i in range(W.getNumVert()):
-    W.setEdge(Edge(i+1,i+1,0))
-
-print(W) #Print the graph itself
-print(Dijkstra(W)) #Should print the set of edges v1->v5, v5->v4, v1->v3, v4->v2
+# should print [(0,4),(4,3),(0,2),(3,1)]
+print(Dijkstra(Example))
